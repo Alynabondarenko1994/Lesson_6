d957955ef5ba2f6a2e612a0f3adaebb9459f0a4e
1 <bondarenko-1994alenka@mail.ru>
2022-06-02 20:42:04 +0300

    Lesson 6

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+
+string[] Array(string message)
+{
+    Console.WriteLine(message);
+    string[] ar = Console.ReadLine().Replace('.', ',').Split(" ");
+    return ar;
+}
+
+void PositiveElement(string[] numer)
+{
+
+    int result = 0;
+    for (int i = 0; i < numer.Length; i++)
+    {
+        if (double.TryParse(numer[i], out double positiv) && positiv > 0)
+            result += 1;
+    }
+    Console.WriteLine($"Количество введенных чисел больше 0: {result}");
+
+}
+
+
+string[] output = Array("Введите числа через пробел:");
+PositiveElement(output);
---
+++ task43/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+ значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+double GetNumber(string str)
+{
+    while (true)
+    {
+        Console.WriteLine(str);
+        string num = Console.ReadLine().Replace('.', ',');
+        if (double.TryParse(num, out double number) == false)
+            Console.WriteLine("Введено не число");
+        else
+            return number;
+    }
+}
+
+
+
+void IntersectionPointLines(double k1, double b1, double k2, double b2)
+{
+
+    double y = (-k2 * b1 + k1 * b2) / (k1 - k2);
+    double x = (y - b1) / k1;
+
+    Console.WriteLine($"Точка пересечения прямых, заданных уравнениями y = {k1} * x + {b1} , y = {k2} * x + {b2} :({x},{y})");
+
+
+}
+Console.WriteLine("Для прямой y = k1 * x + b1 ");
+double k1 = GetNumber("задайте k1: ");
+double b1 = GetNumber("задайте b1: ");
+Console.WriteLine("Для прямой y = k2 * x + b2 ");
+double k2 = GetNumber("задайте k2: ");
+double b2 = GetNumber("задайте b2 :");
+
+IntersectionPointLines(k1, b1, k2, b2);
